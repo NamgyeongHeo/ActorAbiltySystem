33612645f58abd0272e7213ada9c897b35dbfd34
dog <nknk610@gmail.com>
2025-03-12 07:31:30 +0900

    Remove generated code

--- Assets/CodeGenerator/Generated/GameplayTagsList.generated.cs
+++
@@ -1,8 +0,0 @@
-public static class GameplayTagsListConst
-{
-    public const string A = "A";
-    public const string A_B = "A.B";
-    public const string A_B_C = "A.B.C";
-    public const string A_B_D = "A.B.D";
-    public const string A_B_E = "A.B.E";
-}
