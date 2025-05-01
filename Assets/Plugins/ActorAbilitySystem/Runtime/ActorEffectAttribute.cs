03bbd67493dc15833ac78b692258b93ee6c77b82
dog <nknk610@gmail.com>
2025-05-02 08:07:16 +0900

    Remove unused attribute classes

--- Assets/Plugins/ActorAbilitySystem/Runtime/ActorEffectAttribute.cs
+++ Assets/Plugins/ActorAbilitySystem/Runtime/ActorEffectAttribute.cs
@@ -6,62 +6,6 @@ namespace ActorAbilitySystem
 {
     public partial class ActorEffect
     {
-        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-        public class StackPolicyAttribute : Attribute
-        {
-            private readonly EStackDurationPolicy durationPolicy;
-            public EStackDurationPolicy DurationPolicy
-            {
-                get
-                {
-                    return durationPolicy;
-                }
-            }
-
-            private readonly EStackExpirationPolicy expirationPolicy;
-            public EStackExpirationPolicy ExpirationPolicy
-            {
-                get
-                {
-                    return expirationPolicy;
-                }
-            }
-
-            public StackPolicyAttribute(EStackDurationPolicy durationPolicy = EStackDurationPolicy.None, EStackExpirationPolicy expirationPolicy = EStackExpirationPolicy.Clear)
-            {
-                this.durationPolicy = durationPolicy;
-                this.expirationPolicy = expirationPolicy;
-            }
-        }
-
-        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-        public class LifeTimePolicyAttribute : Attribute
-        {
-            private readonly ELifeTimePolicy policy;
-            public ELifeTimePolicy Policy
-            {
-                get
-                {
-                    return policy;
-                }
-            }
-
-            private readonly float duration;
-            public float Duration
-            {
-                get
-                {
-                    return duration;
-                }
-            }
-
-            public LifeTimePolicyAttribute(ELifeTimePolicy policy, float duration = 0f)
-            {
-                this.policy = policy;
-                this.duration = duration;
-            }
-        }
-
         [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
         public class GameplayTagsAttribute : Attribute
         {
